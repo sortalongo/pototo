8085cde75b05f105b4e180b362a525d0bbc5bf03
Dan Sotolongo <dsotolongo@google.com>
2018-04-03 11:27:45 -0700

    implement advancement in fused buffer and prefnprocessor

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
-pub trait Consumer<T> {
+pub trait Merge {
+  fn merge(self, other: Self) -> Self;
+}
+pub trait Consumer<T: Merge> {
   type InK: Copy;
   type KSet;
   fn put(&mut self, k: Self::InK, t: T);
@@ -8,50 +11,72 @@ pub trait Producer<T> {
   type OutK: Copy;
   fn get(&mut self) -> (Self::OutK, T);
 }
-pub trait Processor<I, O> : Consumer<I> + Producer<Option<O>> {
-
-}
-pub trait Buffer<T> : Processor<T, T> {}
+pub trait Processor<I: Merge, O> : Consumer<I> + Producer<Option<O>> {}
+pub trait Buffer<T: Merge> : Processor<T, T> {}
 
 use std::mem;
 
 pub struct FusedBuffer<T> {
-  elem: Option<T>
+  elem: Option<T>,
+  done: bool,
 }
 impl<T> FusedBuffer<T> {
   pub fn new(t: T) -> FusedBuffer<T> {
-      FusedBuffer { elem: Some(t) }
+      FusedBuffer { elem: Some(t), done: false }
   }
   pub fn empty() -> FusedBuffer<T> {
-      FusedBuffer { elem: None }
+      FusedBuffer { elem: None, done: false }
   }
-  pub fn put(&mut self, t: T) {
+  pub fn put(&mut self, t: T) where T: Merge {
       Consumer::put(self, (), t);
   }
 }
-impl<T> Consumer<T> for FusedBuffer<T> {
+impl<T: Merge> Consumer<T> for FusedBuffer<T> {
   type InK = ();
   type KSet = ();
   fn put(&mut self, _k: (), t: T) {
-    mem::replace(&mut self.elem, Some(t));
+    if self.done {
+        panic!("Trying to put into a finished FusedBuffer.");
+    }
+    let tmp =
+      if self.elem.is_some() {
+        let mut prev = None;
+        mem::swap(&mut prev, &mut self.elem);
+        prev.unwrap().merge(t)
+      } else {
+        t
+      };
+    mem::replace(&mut self.elem, Some(tmp));
   }
-  fn advance(&mut self, _ks: ()) {}
+  fn advance(&mut self, _ks: ()) {
+    self.done = true;
+  }
 }
 impl<T> Producer<Option<T>> for FusedBuffer<T> {
   type OutK = ();
   fn get(&mut self) -> ((), Option<T>) {
-    ((), mem::replace(&mut self.elem, None))
+    let ret = if self.done {
+        mem::replace(&mut self.elem, None)
+    } else { None };
+    ((), ret)
   }
 }
-impl<T> Processor<T, T> for FusedBuffer<T> {}
-impl<T> Buffer<T> for FusedBuffer<T> {}
+impl<T: Merge> Processor<T, T> for FusedBuffer<T> {}
+impl<T: Merge> Buffer<T> for FusedBuffer<T> {}
 
+impl Merge for i64 {
+  fn merge(self, other: i64) -> i64 {
+    self + other
+  }
+}
 #[test]
 fn fused_buffer_inserts() {
     let mut b = FusedBuffer::empty();
     assert_eq!(((), None), b.get());
     let i = 5;
     b.put(i);
+    assert_eq!(((), None), b.get());
+    b.advance(());
     assert_eq!(((), Some(i)), b.get());
     assert_eq!(((), None), b.get());
 }
@@ -59,20 +84,20 @@ fn fused_buffer_inserts() {
  use std::marker::PhantomData;
 
 pub struct PreFnProcessor<I, O, F, B>
-    where F: Fn(B::InK, I) -> O, B: Buffer<O> {
+    where O: Merge, F: Fn(B::InK, I) -> O, B: Buffer<O> {
   f: F,
   buf: B,
   // Below are just markers for the compiler to track unused types.
   _in: PhantomData<I>,
   _out: PhantomData<O>,
 }
-impl<I, O, F, B> PreFnProcessor<I, O, F, B>
+impl<I, O: Merge, F, B> PreFnProcessor<I, O, F, B>
     where F: Fn(B::InK, I) -> O, B: Buffer<O> {
   pub fn new(f: F, b: B) -> PreFnProcessor<I, O, F, B> {
       PreFnProcessor { _in: PhantomData, _out: PhantomData, f: f, buf: b }
   }
 }
-impl<I, O, F, B> Consumer<I>
+impl<I: Merge, O: Merge, F, B> Consumer<I>
     for PreFnProcessor<I, O, F, B>
     where F: Fn(B::InK, I) -> O, B: Buffer<O> {
   type InK = B::InK;
@@ -80,9 +105,11 @@ impl<I, O, F, B> Consumer<I>
   fn put(&mut self, k: B::InK, input: I) {
     self.buf.put(k, (self.f)(k, input));
   }
-  fn advance(&mut self, _ks: B::KSet) {}
+  fn advance(&mut self, ks: B::KSet) {
+    self.buf.advance(ks);
+  }
 }
-impl<I, O, F, B> Producer<Option<O>>
+impl<I, O: Merge, F, B> Producer<Option<O>>
     for PreFnProcessor<I, O, F, B>
     where F: Fn(B::InK, I) -> O, B: Buffer<O> {
   type OutK = B::OutK;
@@ -90,7 +117,7 @@ impl<I, O, F, B> Producer<Option<O>>
     self.buf.get()
   }
 }
-impl<I, O, F, B> Processor<I, O>
+impl<I: Merge, O: Merge, F, B> Processor<I, O>
     for PreFnProcessor<I, O, F, B>
     where F: Fn(B::InK, I) -> O, B: Buffer<O> {}
 
@@ -100,6 +127,8 @@ fn prefn_processor_works() {
     assert_eq!(((), None), fn_b.get());
     let i = 5;
     fn_b.put((), i);
+    assert_eq!(((), None), fn_b.get());
+    fn_b.advance(());
     assert_eq!(((), Some(i * 2)), fn_b.get());
     assert_eq!(((), None), fn_b.get());
 }
@@ -115,7 +144,7 @@ impl<T> LinearBuf<T> {
     LinearBuf { deque: VecDeque::new(), current_min: 0 }
   }
 }
-impl<T> Consumer<T> for LinearBuf<T> {
+impl<T: Merge> Consumer<T> for LinearBuf<T> {
   type InK = usize;
   type KSet = (usize, usize); // Half-open, lower-inclusive interval.
   fn put(&mut self, k: usize, t: T) {
@@ -143,7 +172,7 @@ impl<T> Producer<Option<T>> for LinearBuf<T> {
     ((), None)
   }
 }
-impl<T> Processor<T, T> for LinearBuf<T> {}
-impl<T> Buffer<T> for LinearBuf<T> {}
+impl<T: Merge> Processor<T, T> for LinearBuf<T> {}
+impl<T: Merge> Buffer<T> for LinearBuf<T> {}
 
 
